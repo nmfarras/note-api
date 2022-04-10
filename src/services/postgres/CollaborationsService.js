d0a9a0fa2881b7e5a29c15590ca1c9c9336eb5e9
Naufal Muhammad Farras <nmfarras@gmail.com>
2022-04-10 15:46:41 +0700

    refactoring code

--- src/services/postgres/CollaborationsService.js
+++ src/services/postgres/CollaborationsService.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-underscore-dangle */
 const { Pool } = require('pg');
 const { nanoid } = require('nanoid');
 const InvariantError = require('../../exceptions/InvariantError');
@@ -21,7 +20,6 @@ class CollaborationsService {
     if (!result.rows.length) {
       throw new InvariantError('Kolaborasi gagal ditambahkan');
     }
-
     return result.rows[0].id;
   }
 
